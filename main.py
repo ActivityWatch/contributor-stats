889fdce29316279c175b9b47604c4b26831d7f16
Erik Bjäreholt <erik@bjareho.lt>
2017-06-13 11:23:35 +0200

    now prints something reasonable

--- main.py
+++ main.py
@@ -1,21 +1,56 @@
 import sys
+import os
+from typing import Dict
 
-sys.path.append("./gitstats")
+original_cwd = os.getcwd()
+path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(path, "gitstats"))
 
 import gitstats
 
 
+def merge_author(a1, a2):
+    # TODO: Needs to merge more properties
+    a1["lines_added"] += a2["lines_added"]
+    a1["lines_removed"] += a2["lines_removed"]
+
+
+def getAuthorInfos(data) -> Dict[str, dict]:
+    names = data.getAuthors()
+    authorInfos = {name: data.getAuthorInfo(name) for name in names}
+
+    author_merges = [("Johan Bjäreholt", "johan-bjareholt")]
+    for a1_name, a2_name in author_merges:
+        if a1_name in authorInfos and a2_name in authorInfos:
+            a1 = authorInfos.pop(a1_name)
+            a2 = authorInfos.pop(a2_name)
+            authorInfos[a1_name] = merge_author(a1, a2)
+
+    return authorInfos
+
+
 def generateForRepo(path):
+    # TODO: Could use caching to speed up
     data = gitstats.GitDataCollector()
 
-    # Could use caching to speed up
+    # `data.collect` always gets the current directory for whatever reason,
+    # os.chdir works as a workaround
+    os.chdir(path)
     data.collect(path)
+    os.chdir(original_cwd)
 
     data.refine()
 
-    print(data.getActiveDays())
+    print(data.projectname)
+    print("Active days: {}".format(len(data.getActiveDays())))
+
+    print("{name:<21} | {commits:<8} | {adds:<8} | {deletes:<8}".format(name="Name", commits="Commits", adds="Added", deletes="Removed"))
+
+    for author in getAuthorInfos(data):
+        authorInfo = data.getAuthorInfo(author)
+        print("{name:<21} | {commits:<8} | +{lines_added:<7} | -{lines_removed:<7}".format(name=author, **authorInfo))
 
 
 if __name__ == "__main__":
-    for repopath in ["../", "../aw-core"]:
+    for repopath in ["../", "../aw-core", "../aw-server", "../aw-client", "../aw-webui", "../aw-watcher-afk", "../aw-watcher-window"]:
         generateForRepo(repopath)
